1effaf2657fc2da60364e1bce9e8a546b3c518ea
Ben Shaw <benshaw9@gmail.com>
2014-09-04 13:02:10 +0100

    well linux is fun... (case sensitivity)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 require('coffee-script/register');
 var gutil = require('gulp-util');
 
-var gulpfile = 'Gulpfile.coffee';
+var gulpfile = 'gulpfile.coffee';
 gutil.log('Using file', gutil.colors.magenta(gulpfile));
 
 require('./' + gulpfile);
